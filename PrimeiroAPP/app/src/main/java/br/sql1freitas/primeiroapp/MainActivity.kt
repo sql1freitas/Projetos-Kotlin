fc00067997e13e74a963d051bdeb9d5e7a4940ea
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2024-12-25 23:13:46 -0300

    Lógica adicionada para conclusão

--- PrimeiroAPP/app/src/main/java/br/sql1freitas/primeiroapp/MainActivity.kt
+++ PrimeiroAPP/app/src/main/java/br/sql1freitas/primeiroapp/MainActivity.kt
@@ -5,12 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.sql1freitas.primeiroapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.confirmButton.setOnClickListener {
+            val name = binding.editName.text.toString()
+            binding.textView.setText("Olá! ${name}")
+        }
 
         }
     }

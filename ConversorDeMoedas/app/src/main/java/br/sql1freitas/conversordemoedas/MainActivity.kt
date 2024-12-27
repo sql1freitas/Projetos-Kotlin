69d9b2fc473aa40f8e3b713306d0141474ce973d
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2024-12-27 13:48:59 -0300

    Finalização do projeto

--- ConversorDeMoedas/app/src/main/java/br/sql1freitas/conversordemoedas/MainActivity.kt
+++ ConversorDeMoedas/app/src/main/java/br/sql1freitas/conversordemoedas/MainActivity.kt
@@ -5,14 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.sql1freitas.conversordemoedas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       binding.botaoConverter.setOnClickListener {
+           val valor = binding.valor.text.toString().toDouble()
+           val resultado: String = (valor*6.30).toString()
+
+           binding.textoResultado.setText("$" + resultado )
+       }
 
         }
     }

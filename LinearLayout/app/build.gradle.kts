3f5f71f471e96ee9afa74a12bd8038f93fe632ca
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2025-01-01 15:56:31 -0300

    Atualização do projeto

--- LinearLayout/app/build.gradle.kts
+++ LinearLayout/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "br.sql1freitas.linearlayout"
-        minSdk = 21
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"

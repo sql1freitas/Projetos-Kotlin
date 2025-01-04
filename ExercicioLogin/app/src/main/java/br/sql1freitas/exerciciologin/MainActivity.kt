7437c58228944e03727a6d88df928c16739681ae
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2025-01-04 11:26:34 -0300

    Aplicação da lógica

--- ExercicioLogin/app/src/main/java/br/sql1freitas/exerciciologin/MainActivity.kt
+++ ExercicioLogin/app/src/main/java/br/sql1freitas/exerciciologin/MainActivity.kt
@@ -1,20 +1,45 @@
 package br.sql1freitas.exerciciologin
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.sql1freitas.exerciciologin.databinding.ActivityMainBinding
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMain: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        activityMain = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMain.root)
+
+
+    activityMain.buttonLogin.setOnClickListener {
+
+        val username = activityMain.editUsername.text.toString().trim()
+        val password = activityMain.editPassword.text.toString().trim()
+
+        if (username.equals("matheus") && password.equals("1234")) {
+            Toast.makeText(applicationContext, "Acesso Permitido!", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Dados Incorretos!", Toast.LENGTH_SHORT).show()
+        }
+
+        activityMain.editUsername.setText("")
+        activityMain.editPassword.setText("")
+
+    }
+
+
+
         }
     }
-}

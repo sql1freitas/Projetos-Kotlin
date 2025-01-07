bd76c8e08cbf5bb3a61105319137e7ade6650082
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2025-01-07 13:32:27 -0300

    Adição da lógica

--- formulariokotlin/app/src/main/java/br/sql1freitas/formulario_kotlin/ExibicaoActivity.kt
+++ formulariokotlin/app/src/main/java/br/sql1freitas/formulario_kotlin/ExibicaoActivity.kt
@@ -5,16 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.sql1freitas.formulario_kotlin.databinding.ActivityExibicaoBinding
 
 class ExibicaoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityExibicaoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_exibicao)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityExibicaoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val i = intent
+
+        binding.nome.text = i.extras?.getString("nome")
+        binding.idade.text = i.extras?.getInt("idade").toString()
+        binding.ocupacao.text = i.extras?.getString("ocupacao")
+        binding.sexo.text = i.extras?.getString("sexo")
+
+        binding.buttonEnviarDados.setOnClickListener {
+            finish()
         }
+
+
     }
 }
--- formulariokotlin/app/src/main/java/br/sql1freitas/formulario_kotlin/MainActivity.kt
+++ formulariokotlin/app/src/main/java/br/sql1freitas/formulario_kotlin/MainActivity.kt
@@ -1,17 +1,14 @@
 package br.sql1freitas.formulario_kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.sql1freitas.formulario_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
     private lateinit var binding: ActivityMainBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +17,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
+        binding.checkboxMasculino.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                binding.checkboxFeminino.isChecked = false
+            }
+        }
+
+        binding.checkboxFeminino.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                binding.checkboxMasculino.isChecked = false
+            }
+        }
+
+        binding.buttonEnviar.setOnClickListener {
+            var nome = binding.editNome.text.toString()
+            var idade = binding.editIdade.text.toString().toInt()
+            var ocupacao = binding.editOcupacao.text.toString()
+            var sexo = if (binding.checkboxMasculino.isChecked) "masculino" else "feminino"
+            val i = Intent(this, ExibicaoActivity::class.java)
+
+            i.putExtra("sexo", sexo)
+            i.putExtra("nome", nome)
+            i.putExtra("idade", idade)
+            i.putExtra("ocupacao", ocupacao)
 
 
 
-
-
+            startActivity(i)
         }
     }
+    override fun onResume() {
+        super.onResume()
+
+        binding.editNome.text.clear()
+        binding.editIdade.text.clear()
+        binding.editOcupacao.text.clear()
+        binding.checkboxMasculino.isChecked = false
+        binding.checkboxFeminino.isChecked = false
+    }
+}

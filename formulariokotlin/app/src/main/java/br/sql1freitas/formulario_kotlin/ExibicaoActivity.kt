5ca7f33cbfa406f87433cc91f1aa05d8cd824922
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2025-01-07 11:47:19 -0300

    Criação do projeto e layout da primeira activity

---
+++ formulariokotlin/app/src/main/java/br/sql1freitas/formulario_kotlin/ExibicaoActivity.kt
@@ -0,0 +1,20 @@
+package br.sql1freitas.formulario_kotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ExibicaoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_exibicao)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ formulariokotlin/app/src/main/java/br/sql1freitas/formulario_kotlin/MainActivity.kt
@@ -0,0 +1,29 @@
+package br.sql1freitas.formulario_kotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.sql1freitas.formulario_kotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+
+
+        }
+    }

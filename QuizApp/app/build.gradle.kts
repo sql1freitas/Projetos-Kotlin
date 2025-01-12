f9b7e9c8315c9f114568d7a56af4059b2021f797
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2025-01-12 20:01:50 -0300

    Criação do binding e atualização do gradle

--- QuizApp/app/build.gradle.kts
+++ QuizApp/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "br.sql1freitas.quizapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "11"
+    }
+    viewBinding {
+        enable = true
     }
 }
 
